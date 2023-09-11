e9c66739127fa2e96fd1ac17b243672fd30daea2
nikit <nikitka.lyashko@bk.ru>
2023-09-11 20:24:00 +0300

    KOT

--- model.py
+++ model.py
@@ -3,25 +3,7 @@ import random
 import pygame
 pygame.init()
 
-cot=pygame.Rect(450, 850, 150, 150)
-zont=pygame.Rect(cot.right - 80, 1, 100, 100)
-vedro=pygame.Rect(cot.left -20, 1, 70, 70)
-cloud_small=pygame.Rect(200,100,100,100)
-wave=pygame.Rect(0,870,1000,30)
-blue_rect=pygame.Rect(0,900,1000,100)
-plot_rect=pygame.Rect(300,1,250,30)
-# zont.bottom=cot.top+30
-# vedro.bottom=cot.top+45
 
-plot_rect.bottom = wave.top + 30
-cot.bottom=plot_rect.top+10
-cot.centerx=plot_rect.centerx
-vedro.left=cot.left-20
-zont.left = cot.right - 75
-zont.bottom=cot.top+30
-vedro.bottom=cot.top+45
-rect_kaplya = pygame.Rect(400, 180, 25, 25)
-rect_kaplya.centerx=cloud_small.centerx
 
 def water_drop_under_cloud_def():
     global  show_water_drop
@@ -30,17 +12,6 @@ def water_drop_under_cloud_def():
     rect_kaplya.centery=cloud_small.centery
     show_water_drop = True
 
-def height_wave():
-    blue_rect.height = random.randint(300, 900)
-    blue_rect.bottom = 1000
-    wave.bottom = blue_rect.top
-    plot_rect.bottom = wave.top + 10
-    cot.bottom = plot_rect.top + 10
-    cot.centerx = plot_rect.centerx
-    vedro.left = cot.left - 20
-    zont.left = cot.right - 75
-    zont.bottom = cot.top + 30
-    vedro.bottom = cot.top + 45
 
 
 
@@ -52,6 +23,48 @@ pygame.time.set_timer(free_type,3000,0)
 razvorot_kota="111"
 ride_cloud_right=True
 show_water_drop=True
+
+
+def right():
+    global razvorot_kota
+    if vedro.right<=1000:
+
+        razvorot_kota = "mirror"
+        vedro.right+=20
+        mirror_pit_right()
+
+
+def left():
+    global razvorot_kota
+    if vedro.left >= 0:
+
+        razvorot_kota = "111"
+        vedro.left-=20
+        mirror_pit_left()
+
+
+def mirror_pit_left():
+
+    cot.left=vedro.right-45
+    zont.right=cot.right+20
+    plot_rect.centerx = cot.centerx
+
+def mirror_pit_right():
+
+
+    cot.right=vedro.left+45
+    zont.left=cot.left-20
+    plot_rect.centerx = cot.centerx
+
+
+def height_wave():
+
+    blue_rect.bottom = 1000
+    wave.bottom = blue_rect.top
+    plot_rect.bottom = wave.top + 10
+    cot.bottom = plot_rect.top + 10
+    zont.bottom = cot.top + 30
+    vedro.bottom = cot.top + 45
 def ride_cloud():
     global ride_cloud_right,show_water_drop
 
@@ -83,35 +96,17 @@ def ride_cloud():
         if cloud_small.left<=0:
             ride_cloud_right=True
 
-def right():
-    global razvorot_kota
-    if vedro.right<=1000:
-        cot.right+=5
-        zont.right+=5
-        vedro.right+=5
-        mirror_pit_right()
-        razvorot_kota = "mirror"
+cot=pygame.Rect(450, 850, 150, 150)
+zont=pygame.Rect(cot.right - 80, 1, 100, 100)
+vedro=pygame.Rect(cot.left -20, 1, 70, 70)
+cloud_small=pygame.Rect(200,100,100,100)
+wave=pygame.Rect(0,870,1000,30)
+blue_rect=pygame.Rect(0,900,1000,100)
+plot_rect=pygame.Rect(300,1,250,30)
 
 
-def left():
-    global razvorot_kota
-    if vedro.left >= 0:
 
-        cot.left-=5
-        zont.right-=5
-        vedro.left-=5
-        mirror_pit_left()
-        razvorot_kota = "111"
-
-
-def mirror_pit_right():
-
-    cot.left=vedro.left-100
-    zont.left=cot.right-170
-
-
-def mirror_pit_left():
-
-    cot.left=vedro.right-50
-    zont.left=cot.right-75
-
+mirror_pit_left()
+height_wave()
+rect_kaplya = pygame.Rect(400, 180, 25, 25)
+rect_kaplya.centerx=cloud_small.centerx
